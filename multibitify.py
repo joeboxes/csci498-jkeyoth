64e9441f6da1bb0c1aa004bd3c8a0694848e63ce
Josh Keyoth <jkeyoth@mines.edu>
2011-01-20 21:42:33 -0700

    Completed Not16.hdl. Yay automation\!

--- multibitify.py
+++ multibitify.py
@@ -18,38 +18,46 @@ And(a=a[1], b=b[1], out=out[1]);
 
 import sys
 
-fname = sys.argv[1];
+
 
 
 real_stdout = sys.stdout
 
+if len(sys.argv) == 0:
+	print "usage: multibitify srcFile outFile"
+else:
+	fname = sys.argv[1]
+	f = open(fname)
 
-try:
-	hdlFname = sys.argv[2];
-	hdl = open(hdlFname, "a")
-except:
-	hdl = sys.stdout;
+	try:
+		hdlFname = sys.argv[2];
+		hdl = open(hdlFname, "a")
+	except:
+		hdl = sys.stdout;
 	
-f = open(fname)
 
 
-lines = []
 
-out = ""
-for l in f:
-	if len(l) > 2:
-		lines.append(l)
+	lines = []
+
+	out = ""
+	for l in f:
+		if len(l) > 2:
+			lines.append(l)
 		
-for i in range(16):
-	for l in lines:
-		t = l
-		a = t.find(",")
-		while a != -1:
+	for i in range(16):
+		for l in lines:
+			t = l
+			a = t.find(",")
+			while a != -1:
+				t = t[:a] + "[" + str(i) + "]" + t[a:]
+				a = t.find(",", a+5)
+			a=t.find(")")
 			t = t[:a] + "[" + str(i) + "]" + t[a:]
-			a = t.find(",", a+5)
-		a=t.find(")")
-		t = t[:a] + "[" + str(i) + "]" + t[a:]
-		hdl.write(t)
-	hdl.write("\n")
+			hdl.write("\t" + t)
+		if len(lines) > 1:
+			hdl.write("\n")
 
-sys.stdout = real_stdout
+	sys.stdout = real_stdout
+
+	print "Now go move the squigily bracket (}) in the .hdl and you're good!"
