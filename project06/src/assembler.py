ab7cd51d2623345b59a131a866ff9c2355f6fbb2
Josh Keyoth <jkeyoth@mines.edu>
2011-02-22 21:12:32 -0700

    assembler correctly opens input and output files

---
+++ project06/src/assembler.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import os
+import os.path as path
+
+#determine input and output files.
+
+#get path to input file from command line arguments
+if len(sys.argv) != 2:
+	print "Usage: [python] assembler pathToAsm.asm"
+	exit(1)
+inPath = sys.argv[1]
+
+#get the fileName without extension, and the extension from the supplied path
+fileName, extension = path.splitext(inPath)
+
+#make sure the supplied file is a .asm file
+if extension != ".asm":
+	print "Input file must be a .asm file."
+	print "Usage: [python] assembler pathToAsm.asm"
+	exit(1)
+
+#make the output path
+outPath = fileName + ".hack"
+
+#open the input file. fail nicely if open fails
+try:
+	inFile = open(inPath)
+except:
+	print inPath + ": file not found"
+
+outFile = open(outPath, "w")
+
+outFile.write("TEST\n")
+
