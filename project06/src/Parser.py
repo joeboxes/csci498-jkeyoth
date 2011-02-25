f401c4f1dee0910b2392907f192a65801ed89b7d
Josh Keyoth <jkeyoth@mines.edu>
2011-02-25 14:52:36 -0700

    Parser half implemented

--- project06/src/Parser.py
+++ project06/src/Parser.py
@@ -4,30 +4,55 @@ class Parser:
 	"""Class to parse the assembly code."""
 	
 	A_COMMAND = 1
-	B_COMMAND = 2
+	C_COMMAND = 2
 	L_COMMAND = 3
 	
+	
+	
 	def __init__(self, asmName):
-		"""Constructor. Opens the input assembly file"""
+		"""Constructor. Opens the input assembly file and reads it into an array of commands"""
 		asmFile = open(asmName)
+		self.commands = []
+		self.curCommand = 0
+		for line in asmFile:
+			self.commands.append(line)
 			
 	
 	def hasMoreCommands(self):
 		"""Check if there are more commands to parse"""
-		pass
+		return self.curCommand < len(self.commands) - 1
+			
 	
 	def advance(self):
 		"""Set the current command to the next command, if there is a next command"""
-		pass
+		self.curCommand += 1
 	
 	def commandType(self):
-		"""Returns the type of the current command. One of A_COMMAND, B_COMMAND, L_COMMAND."""
-		pass
+		"""Returns the type of the current command. One of A_COMMAND, C_COMMAND, L_COMMAND."""
+		firstChar = self.commands[self.curCommand][0]
+		if firstChar == "@":
+			return Parser.A_COMMAND
+		if firstChar == "(":
+			return Parser.L_COMMAND
+		possCComStarts = ["A", "D", "M", "-", "0", "1", "!"]
+		if possCComStarts.count(firstChar) > 0:
+			return Parser.C_COMMAND
+		
+		print "Bad assembly command :", self.commands[self.curCommand]
+		exit(1)
 	
 	def symbol(self):
 		"""Returns the value or variable after the @ in an A command. One of: constant
-		value such as 13252, variable such as sum, or symbol such as (LOOP). """
-		pass
+		value such as 13252, variable such as sum, or symbol such as LOOP for (LOOP). """
+		if self.commandType() == Parser.C_COMMAND:
+			return None;
+		
+		#return everything after the @
+		if self.commandType() == Parser.A_COMMAND:
+			return self.commands[self.curCommand][1:]
+		
+		#return everything inside of the ()
+		return self.commands[self.curCommand][1:-1]
 	
 	def dest(self):
 		"""Returns the string representation of the dest portion of the C command, such as D or AD"""
