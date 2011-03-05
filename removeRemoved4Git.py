0998456855b618f4cb99b0f12ce349c507d7e882
Josh Keyoth <jkeyoth@mines.edu>
2011-03-04 23:42:27 -0700

    Woot it removes things\!

--- removeRemoved4Git.py
+++ removeRemoved4Git.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import os
 
 os.system("git status --porcelain > porc.out")
@@ -10,4 +11,7 @@ for l in porc:
 	if l.count(" D ") > 0:
 		lines.append(l.split()[1])
 		
-print lines
+for l in lines:
+	os.system("git rm " + l)
+
+print "Hope you didnt lose anything useful..."
