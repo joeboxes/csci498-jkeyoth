d850e65c7528de3abcf018516b5e02953603e1a0
Richie Zirbes <zirbsster@gmail.com>
2011-04-14 22:43:28 -0600

    'correctly' compiles Main.jack from Square - however it doesn't really do any checks if the input is bad...

--- project10/CompilationEngine.rb
+++ project10/CompilationEngine.rb
@@ -177,18 +177,27 @@ printV("compileStatements\n")
 		return true
 	end
 	def compileStatement() # letStatement | ifStatement | whileStatement | doStatement | returnStatement
-printV("compileStatement\n")
+printV("compileStatement---\n")
 		if checkSameValue("let")
-			compileLet()
+			return compileLet()
 		elsif checkSameValue("do")
-			compileDo()
+			return compileDo()
 		elsif checkSameValue("return")
-			compileReturn()
+			return compileReturn()
 		end
 		return false
 	end
 	def compileDo()
 printV("compileDo\n")
+		if checkSameValue("do")
+			@tokenizer.advance
+			if compileSubroutineCall()
+				if checkSameValue(";")
+					@tokenizer.advance
+					return true
+				end
+			end
+		end
 		return false
 	end
 	def compileLet() # 'let' varName ('[' expression ']') '=' expression ';'
@@ -210,13 +219,12 @@ printV("compileLet\n")
 					end
 				end
 				if checkSameValue("=")
+					@tokenizer.advance
 					ret = compileExpression()
-					if true
+					if checkSameValue(";")
 						@tokenizer.advance
-						if checkSameValue(";")
-printV("let successful\n")
-							return true
-						end	
+printV("--------------let successful\n")
+						return true
 					end
 				end
 			end
@@ -229,7 +237,14 @@ printV("let successful\n")
 	end
 	def compileReturn()
 printV("compileReturn\n")
-		#
+		if checkSameValue("return")
+			@tokenizer.advance
+			compileExpression()
+			if checkSameValue(";")
+				@tokenizer.advance
+				return true
+			end
+		end
 		return false
 	end
 	def compileIf()
@@ -239,17 +254,30 @@ printV("compileReturn\n")
 	def compileExpression() # term (op term)*
 printV("compileExpression\n")
 		if compileTerm()
-			@tokenizer.advance
-			while compileOp()
-				compileTerm()
+			ret = compileTerm()
+			while ret
+				ret = false
+				if compileOp()
+					ret = compileTerm()
+				end
 			end
-			@tokenizer.retract
+			# @tokenizer.retract
 			return true
 		end
 		return false
 	end
-	def compileExpressionList(adv)
-		#
+	def compileExpressionList() # ( expression , (',' expression)* )?
+printV("compileExpressionList\n")
+		ret = compileExpression()
+		while ret
+			ret = false
+			if checkSameValue(",")
+				@tokenizer.advance
+				ret = compileExpression()
+			end 
+		end
+printV("endEL\n")
+		return true
 	end
 	def compileTerm() # integerConstant | stringConstant | keywordConst | varName ('[' expression ']')?
 					# | subroutineCall | ('(' expression ')') | unaryOp term
@@ -260,9 +288,22 @@ printV("compileTerm\n")
 			return true
 		elsif compilekeyWordConstant()
 			return true
-		elsif checkSameType("varName")
+		elsif compileSubroutineCall()
+printV("sub success \n")
+			return true
+		elsif checkSameValue("(")
 			@tokenizer.advance
+			if compileExpression()
+				if checkSameValue(")")
+					@tokenizer.advance
+					return true
+				end
+			end
+		elsif compileUnaryOp()
+			return true
+		elsif compileVarName()
 			if checkSameValue("[")
+				@tokenizer.advance
 				if compileExpression()
 					if checkSameValue("]")
 						@tokenizer.advance
@@ -271,15 +312,44 @@ printV("compileTerm\n")
 				else
 					return false
 				end
+			else
+				@tokenizer.retract
 			end
 			return true
-		elsif compileSubroutineCall()
-			return true
 		end
+printV("no match\n")
 		return false
 	end
-	def compileSubroutineCall() # subroutineName '(' expressionList ')' | (className | varName) '.' subroutineName '(' expressionList ')'
-		
+	def compileSubroutineCall() # ((className | varName) '.')? subroutineName '(' expressionList ')'
+printV("compileSubroutineCall\n")
+		ret = compileClassName()
+printV("ret=#{ret}\n")
+		if !ret
+			ret = compileVarName()
+		end
+printV("ret=#{ret}\n")
+		if ret
+			if checkSameValue(".")
+				@tokenizer.advance
+			else
+				@tokenizer.retract
+				#return false
+			end
+		end
+		if compileSubroutineName()
+			if checkSameValue("(")
+				@tokenizer.advance
+printV("found ( \n")
+				if compileExpressionList()
+printV("found expr \n")
+					if checkSameValue(")")
+printV("found ) \n")
+						@tokenizer.advance
+						return true
+					end
+				end
+			end
+		end
 		return false
 	end
 	def compileOp() # '+' | '-' | '*' | '/' | '&' | '|' | '<' | '>' | '='
