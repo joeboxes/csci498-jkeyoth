b07eb530b6e5ed351d99b351a23b5c1fa6fc79f8
Josh Keyoth <jkeyoth@mines.edu>
2011-04-16 18:05:06 -0600

    Diffs pass. Woots, its done

---
+++ project10/gdiff.sh
@@ -0,0 +1,9 @@
+git diff Square/Main.xml testVersions/Square/Main.xml
+git diff Square/Square.xml testVersions/Square/Square.xml
+git diff Square/SquareGame.xml testVersions/Square/SquareGame.xml
+
+git diff ExpressionlessSquare/Main.xml testVersions/ExpressionlessSquare/Main.xml
+git diff ExpressionlessSquare/Square.xml testVersions/ExpressionlessSquare/Square.xml
+git diff ExpressionlessSquare/SquareGame.xml testVersions/ExpressionlessSquare/SquareGame.xml
+
+git diff ArrayTest/Main.xml testVersions/ArrayTest/Main.xml
